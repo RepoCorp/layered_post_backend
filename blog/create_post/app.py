523b74d978a46bfe2792cbb6a84863506d8b3810
Catalina De la cuesta <delacuesta@hotmail.com>
2023-08-28 18:36:12 -0500

    Changes

--- blog/create_post/app.py
+++ blog/create_post/app.py
@@ -1,33 +1,28 @@
 import json
+import os
 
+import boto3 as boto3
 import pymongo
 
-# db_client = None
-# db_secret_name = os.environ['DB_SECRET_NAME']
+
+secret_key = os.environ['SECRET_KEY']
 # pem_locator ='/opt/python/rds-combined-ca-bundle.pem'
 pem_locator = 'global-bundle.pem'
 from bson.objectid import ObjectId
 
 def lambda_handler(event, context):
 
-    secret_name = "layered_blog_db"
     region_name = "us-east-1"
 
-    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
     )
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_key
+    )
 
     # Decrypts secret using the associated KMS key.
     secret = get_secret_value_response['SecretString']
